7ac9e3b8e75c1470b760e29a58f397b79b70d742
Кирилл <medved3loy@yandex.ru>
2023-02-18 00:44:06 +0500

    Fixing the error handler

--- src/Backend/Django/config/url/views.py
+++ src/Backend/Django/config/url/views.py
@@ -7,16 +7,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def processing_url(request):
-    global res
     if request.method == 'POST':
-        try:
-            url = request.POST.get('url')
-        except AttributeError:
-            return HttpResponseBadRequest('Link verification error')
-
-        try:
-            res = check_url.check_link(url)
-        except:
-            return HttpResponseBadRequest("Invalid link")
-
+        url = request.POST.get('url')
+        if len(url) == 0:
+            return HttpResponseBadRequest('Link is null error')
+        else:
+            try:
+                res = check_url.check_link(url)
+            except:
+                return HttpResponseBadRequest("Invalid link")
     return JsonResponse(res)
