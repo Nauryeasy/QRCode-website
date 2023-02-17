8a8a0e880e670f358e0baba1b1f8ae2397d8cc21
Кирилл <medved3loy@yandex.ru>
2023-02-17 23:32:39 +0500

    The code processes the POST request!

--- src/Backend/Django/config/url/views.py
+++ src/Backend/Django/config/url/views.py
@@ -9,11 +9,10 @@ from django.views.decorators.csrf import csrf_exempt
 def processing_url(request):
     global res
     if request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        if isinstance(data, dict):
-            url = data.get('url', None)
-            if url is not None:
-                res = check_url.check_link(url)
+        url = request.POST.get('url')
+        print(url)
+        if url is not None:
+            res = check_url.check_link(url)
         else:
             return f'Link verification error'
 
