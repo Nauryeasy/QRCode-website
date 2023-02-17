7beafd9693bd5c568ac7bde438662f365df54552
Ivan_Karavaev <koertyf@gmail.com>
2023-02-17 19:08:36 +0400

    Urls 2.0

--- src/Backend/Django/config/config/settings.py
+++ src/Backend/Django/config/config/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qr_code',
+    'url',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
