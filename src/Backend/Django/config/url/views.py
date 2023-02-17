d0d023f1c3fcf63f6bee85d4958e3b32a075409d
Ivan_Karavaev <koertyf@gmail.com>
2023-02-17 19:35:27 +0400

    Я не знаю, что писать :(

--- src/Backend/Django/config/url/views.py
+++ src/Backend/Django/config/url/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import json
 
-# Create your views here.
+
+
+def processing_url(request):
+    link = ""
+    if request.method == 'POST':
+        json_data = request.body.decode('utf-8')
+        data_dict = json.loads(json_data)
+        link = data_dict["url"]
+
+    if link != "":
+        pass
