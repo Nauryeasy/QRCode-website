ba91bb5393780af6b554b5c76f41f8ebafea492b
Ivan_Karavaev <koertyf@gmail.com>
2023-02-17 21:42:08 +0400

    I dont know that I fix

--- src/Backend/Django/config/url/views.py
+++ src/Backend/Django/config/url/views.py
@@ -8,14 +8,11 @@ from django.views.decorators.csrf import csrf_exempt
 def processing_url(request):
     global data_dict
     if request.method == 'POST':
-        json_data = request.body.decode('utf-8')
-        data_dict = json.loads(json_data)
-
-    try:
-        link = data_dict["url"]
-        stats = check_url.check_link(link)
-    except:
-        return "Empty Dictionary error"
+        try:
+            link = request.POST.get("url")
+            stats = check_url.check_link(link)
+        except:
+            return "Empty Dictionary error"
 
     if len(stats) == 0:
         return "Link verification error"
