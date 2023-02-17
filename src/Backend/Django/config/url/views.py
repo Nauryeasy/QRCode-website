33ccdceafdd6a7f43ae909aa6dd783b8e881ba9f
Ivan_Karavaev <koertyf@gmail.com>
2023-02-17 19:51:10 +0400

    Update of url.views.py

--- src/Backend/Django/config/url/views.py
+++ src/Backend/Django/config/url/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import json
-
+from src.Backend.Scripts.check_url import check_link
 
 
 def processing_url(request):
@@ -8,7 +8,15 @@ def processing_url(request):
     if request.method == 'POST':
         json_data = request.body.decode('utf-8')
         data_dict = json.loads(json_data)
-        link = data_dict["url"]
 
-    if link != "":
-        pass
+    link = data_dict["url"]
+
+    try:
+        stats = check_link(link)
+    except:
+        return "Error"
+
+    if len(stats) == 0:
+        return "Error"
+
+    return stats
