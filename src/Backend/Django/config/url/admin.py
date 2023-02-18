dcb5ccfdb1f8880f9017e7434fe8c4dbbd7ef2da
Ivan_Karavaev <koertyf@gmail.com>
2023-02-18 20:01:26 +0400

    New model in bd

--- src/Backend/Django/config/url/admin.py
+++ src/Backend/Django/config/url/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import  notable_links
 
-# Register your models here.
+admin.site.register(notable_links)
--- src/Backend/Django/config/url/models.py
+++ src/Backend/Django/config/url/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class notable_links(models.Model):
+    url = models.URLField('Ссылка')
