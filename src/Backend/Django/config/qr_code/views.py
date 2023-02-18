3b5d9114b7eff9cdd7971446869bbdae57f2adf8
Кирилл <medved3loy@yandex.ru>
2023-02-18 20:34:38 +0500

    Adding an error handler to QR_code

--- src/Backend/Django/config/qr_code/views.py
+++ src/Backend/Django/config/qr_code/views.py
@@ -11,16 +11,22 @@ from . import tranlete_qr_code
 def processing_url(request):
     if request.method == 'POST':
         url_blob = request.POST.get('url')
-
-        get_qr_code.get_png_from_blob(url_blob)
-
-        url = tranlete_qr_code.get_link_qr_code()
-
-        if len(url) == 0:
-            return HttpResponseBadRequest('Link is null error')
+        if len(url_blob) == 0:
+            return HttpResponseBadRequest('Request is null error')
         else:
             try:
-                res = check_url.check_link(url)
+                get_qr_code.get_png_from_blob(url_blob)
             except:
-                return HttpResponseBadRequest("Invalid link")
+                return HttpResponseBadRequest('Invalid blob')
+            try:
+                url = tranlete_qr_code.get_link_qr_code()
+            except:
+                return HttpResponseBadRequest('Invalid QR-code')
+            if len(url) == 0:
+                return HttpResponseBadRequest('Link is null error')
+            else:
+                try:
+                    res = check_url.check_link(url)
+                except:
+                    return HttpResponseBadRequest("Invalid link")
     return JsonResponse(res)
